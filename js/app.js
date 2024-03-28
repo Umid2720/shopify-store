28aaf308f9ebeef3b7eabe33a913b1148859608d
Umid2720 <umidbekruzmetov309@gmail.com>
2024-03-28 07:06:43 +0500

    feat (app.js): added logic for mobile menu btn click and unclick

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
-// const menu = document.getElementById('menu');
-// const menuBtn = document.getElementById('burger-menu');
+const menu = document.getElementById('mobile-menu');
+const menuBtn = document.getElementById('burger-menu');
+const spans = document.querySelectorAll('#burger-menu span');
 
-// menuBtn.addEventListener('click', () => {
-//     menu.style.display = 'flex'
-//     menu.classList.add('navbar-mobile')
-//     console.log(navbar.classList);
-// })
+menuBtn.addEventListener('click', () => {
+
+    if (menu.classList.contains('down')) {
+        spans[1].classList.remove('hidden');
+        menu.classList.remove('down');
+        menuBtn.style.position = 'absolute'
+
+
+        spans.forEach(span => {
+            span.style.transform = "rotate(0)";
+            span.style.backgroundColor = "#000";
+        });
+
+        spans[2].style.top = '0';
+
+        return 0;
+    }
+
+    spans[0].style.transform = "rotate(-45deg)";
+    spans[1].classList.add('hidden');
+    spans[2].style.transform = "rotate(45deg)";
+    spans[2].style.top = "-6px";
+
+    spans.forEach(span => {
+        span.style.backgroundColor = "#fff"
+    });
+
+    menu.classList.add('down')
+    menuBtn.style.position = 'fixed'
+
+})
 
